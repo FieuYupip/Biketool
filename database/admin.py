8727a004ce1134061ccbbb2b35ab451b250f276d
hienhajason <osx@OSX-MacBook-Pro.wlan>
2019-09-23 12:13:16 +0300

    admin site modify

--- database/admin.py
+++ database/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import tool, toolbox, apt_building, user, bike_company, bike_video
 
 # Register your models here.
+admin.site.register(tool)
+admin.site.register(toolbox)
+admin.site.register(apt_building)
+admin.site.register(user)
+admin.site.register(bike_company)
+admin.site.register(bike_video)
