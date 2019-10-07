02eee87d47617c19ac2b584ba9fc93b5c590e09c
hienhajason <osx@OSX-MacBook-Pro.wlan>
2019-10-07 23:15:20 +0300

    HTML UPDATE

--- database/views.py
+++ database/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request)
-    return render(request,"database/home.html")
+    return render(request, "database/home.html")
+ 
