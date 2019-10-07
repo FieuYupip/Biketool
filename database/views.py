e6593dcce7cb090ae6ac429f86d9881fa9c6c1b2
hienhajason <osx@OSX-MacBook-Pro.wlan>
2019-10-07 23:31:34 +0300

    HTML update

--- database/views.py
+++ database/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request)
-    return render(request, "database/home.html")
+def index(request):
+    name_of_service = "biketool rental service"
+    return render(request, "database/home.html", {'name': name_of_service} )
  
